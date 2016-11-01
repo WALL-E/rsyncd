070a03b33f6edb1a1311f67079f32d2c76179a07
zhangzheng <zzgigi2003@163.com>
2016-11-01 09:52:17 +0000

    优化安装脚本

--- init.sh
+++ init.sh
@@ -13,3 +13,5 @@ adduser mysql 2>/dev/null
 
 ipaddr=`ip route get 202.106.0.20|grep "202.106.0.20"|awk '{print $7}'`
 sed -i "s/^address.*$/address = ${ipaddr}/" rsyncd.conf
+
+chmod 600 rsyncd.secrets
