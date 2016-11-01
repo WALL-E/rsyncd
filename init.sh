ced085da570a673a7a77d91cf2f9bf8541a33410
zhangzheng <zzgigi2003@163.com>
2016-11-01 08:57:54 +0000

    自动配置IP地址

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-adduser mysql
+adduser mysql 2>/dev/null
+
+ipaddr=`ip route get 202.106.0.20|grep "202.106.0.20"|awk '{print $7}'`
+sed -i "s/^address.*$/address = ${ipaddr}/" rsyncd.conf
