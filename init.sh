53efcdadcbe61544e598822ea6b09ef3935285a2
zhangzheng <zzgigi2003@163.com>
2016-11-01 09:02:03 +0000

    增加权限检查

--- init.sh
+++ init.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+role=`id -u`
+if test $role -ne 0
+then
+    echo "Initialization which requires root privileges"
+    exit 1
+fi
+
+(command -v rsync || echo "please install rsync";exit 1)
+
 adduser mysql 2>/dev/null
 
 ipaddr=`ip route get 202.106.0.20|grep "202.106.0.20"|awk '{print $7}'`
